a4e5dc87d1c0be4f18d49b4e7352c9b077e47797
SurajGupta07 <48823022+SurajGupta07@users.noreply.github.com>
2021-01-03 17:52:38 +0530

    adding different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
+import logo from './math.svg';
 
 export default function App() {
+
+  let [triangleValue, setTriangleValue] = useState("")
 
   let firstAngle, secondAngle, thirdAngle;
 
@@ -16,19 +20,44 @@ export default function App() {
   }
 
   function sumCalculator(event){
-    var sum = firstAngle + secondAngle + thirdAngle;
-    console.log(sum)
+    var sum = Number(firstAngle) + Number(secondAngle) + Number(thirdAngle);
+    if(sum >= 180){
+      setTriangleValue("Hurray!, It's a triangle");
+    }
+    else{
+      setTriangleValue("Another Try?, It's not a Triangle")
+    }
   }
 
   return (
     <div className="App">
-      <h1>TRY-ANGLES</h1>
+      <header>
+        <div>
+        <h1>TRY-ANGLES</h1>
+          <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href=''>Tri Calculator</a></li>
+          </ul>
+        </div>
+      </header>
       <div>
+        <img className="img" src={logo} alt="TRY_ANGLES"/>
+      </div>
+      <div>
+        <h2 id="inputMessage">Input Three Angles below</h2>
         <input className="numberInput" onChange={angleOne} type="number"></input>
         <input className="numberInput" onChange={angleTwo} type="number"></input>
         <input className="numberInput" onChange={angleThree} type="number"></input>
       </div>
       <button className="inputBtn" onClick={sumCalculator}>CALCULATE</button>
+      <p>{triangleValue}</p>
+      <footer>
+        <ul className="socialList">
+          <li className="socialProfile"><a href="">LinkedIn</a></li>
+          <li className="socialProfile"><a href="">Twitter</a></li>
+          <li className="socialProfile"><a href="">Instagram</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
