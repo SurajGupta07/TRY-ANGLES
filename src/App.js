a972b7802ad5501da6c79cddc8cd34c0d562c3d1
SurajGupta07 <48823022+SurajGupta07@users.noreply.github.com>
2021-01-03 15:17:53 +0530

    Reading input values

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+
+  let firstAngle, secondAngle, thirdAngle;
+
+  function angleOne(event){
+    firstAngle = (event.target.value);
+  }
+  function angleTwo(event){
+    secondAngle = (event.target.value);
+  }
+  function angleThree(event){
+    thirdAngle = (event.target.value);
+  }
+
+  function sumCalculator(event){
+    var sum = firstAngle + secondAngle + thirdAngle;
+    console.log(sum)
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>TRY-ANGLES</h1>
       <div>
-        <input className="numberInput" type="number"></input>
-        <input className="numberInput" type="number"></input>
-        <input className="numberInput" type="number"></input>
+        <input className="numberInput" onChange={angleOne} type="number"></input>
+        <input className="numberInput" onChange={angleTwo} type="number"></input>
+        <input className="numberInput" onChange={angleThree} type="number"></input>
       </div>
+      <button className="inputBtn" onClick={sumCalculator}>CALCULATE</button>
     </div>
   );
 }
